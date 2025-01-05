141ee8ed77967cd1e65603263f550b548bee469f
Pedro Igor <peuigor09@gmail.com>
2025-01-05 19:05:31 -0300

    Correção da configuração do angular.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
-    production: false,
-    apiJavaUrl: 'http://localhost:8080', // Por exemplo
-    apiPythonUrl: 'http://localhost:5000' // Exemplo
-  };
-  
+  production: false,
+  apiJavaUrl: 'http://localhost:8080',
+  apiPythonUrl: 'http://localhost:8081'
+};
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment.prod';
+
+console.log('Ambiente de produção:', environment.production);
 
 bootstrapApplication(AppComponent, {
   providers: [
