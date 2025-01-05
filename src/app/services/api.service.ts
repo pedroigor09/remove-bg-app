7156fade429a92ba9812f09f41ef602211c03984
Pedro Igor <peuigor09@gmail.com>
2025-01-05 18:37:34 -0300

    Adicionando o api.service.ts e ajustando o angular.json

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 // src/app/services/api.service.ts
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../environments/environment.prod'; // Certifique-se de usar .prod
+import { environment } from '../../environments/environment.prod';  // Certifique-se de estar importando o .prod
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +10,12 @@ export class ApiService {
   private apiJavaUrl = environment.apiJavaUrl;
   private apiPythonUrl = environment.apiPythonUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    console.log('API Java URL:', this.apiJavaUrl);  // Adicionando log para ver a URL usada
+    console.log('API Python URL:', this.apiPythonUrl);
+  }
 
   removeBackground(imageData: any) {
     return this.http.post(`${this.apiPythonUrl}/remove-background`, imageData);
   }
-
-  // Adicione outras funções que se comunicam com o backend Java se necessário
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.ts
+export const environment = {
+    production: false,
+    apiJavaUrl: 'http://localhost:8080', // Por exemplo
+    apiPythonUrl: 'http://localhost:5000' // Exemplo
+  };
+  
