ebfda834f6cb627a3b97587e3abe12acd286a618
Pedro Igor <peuigor09@gmail.com>
2025-01-05 17:44:42 -0300

    Atualização das URLs dos backends para produção

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,3 +1,4 @@
+// src/app/services/api.service.ts
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.prod'; // Importa o ambiente de produção
@@ -6,11 +7,14 @@ import { environment } from '../../environments/environment.prod'; // Importa o 
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = environment.apiUrl;
+  private apiJavaUrl = environment.apiJavaUrl;
+  private apiPythonUrl = environment.apiPythonUrl;
 
   constructor(private http: HttpClient) {}
 
   removeBackground(imageData: any) {
-    return this.http.post(`${this.apiUrl}/images/remove-background`, imageData);
+    return this.http.post(`${this.apiPythonUrl}/images/remove-background`, imageData);
   }
+
+  // Adicione outras funções que se comunicam com o backend Java se necessário
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://backdropzeropython-ae968c2b2bd2.herokuapp.com/api'
+  apiJavaUrl: 'https://backdropzero-bcaeec861243.herokuapp.com/api',
+  apiPythonUrl: 'https://backdropzeropython-ae968c2b2bd2.herokuapp.com/api'
 };
