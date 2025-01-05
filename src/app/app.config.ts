fb7e7fd75c19bcd77c5ba677da03b06ca642043b
Pedro Igor <peuigor09@gmail.com>
2025-01-05 17:48:44 -0300

    Adição do serviço API e atualização das URLs dos backends para produção

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ApiService } from './services/api.service'; // Importa o serviço
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: []
+  providers: [ApiService], // Adiciona o serviço aos providers
+  bootstrap: []
 })
 export class AppModule { }
