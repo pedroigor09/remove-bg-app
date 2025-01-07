07e132ce3868671ce4643215c447a1846d9a9e41
Pedro Igor <peuigor09@gmail.com>
2025-01-06 23:08:36 -0300

    Atualizar URLs das APIs no environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiJavaUrl: 'https://backdropzero-bcaeec861243.herokuapp.com',
-  apiPythonUrl: 'https://backdropzeropython-ae968c2b2bd2.herokuapp.com'
+  apiJavaUrl: 'https://backdropzerojava-a486c6715e3c.herokuapp.com/',
+  apiPythonUrl: 'https://novo-backdropzeropython-1aed9d9238b9.herokuapp.com/'
 };
